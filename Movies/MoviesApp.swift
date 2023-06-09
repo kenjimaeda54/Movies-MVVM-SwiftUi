47dbf6546f69ed56450ba2ba09f8da0c5577e9cb
Kenji <31631601+kenjimaeda54@users.noreply.github.com>
2023-06-08 21:40:40 -0300

    Initial Commit

---
+++ Movies/MoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesApp.swift
+//  Movies
+//
+//  Created by kenjimaeda on 08/06/23.
+//
+
+import SwiftUI
+
+@main
+struct MoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
